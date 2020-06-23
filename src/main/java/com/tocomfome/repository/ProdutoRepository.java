e3005d568a5c3ddcb1d48665a57d345bb789dbe1
andremoreirasmo <andreluismoreirasmosmo@gmail.com>
2020-06-23 20:38:43 -0300

    Doc: Adicionado metodo que retorna os produtos que estao ativos

--- src/main/java/com/tocomfome/repository/ProdutoRepository.java
+++ src/main/java/com/tocomfome/repository/ProdutoRepository.java
@@ -1,8 +1,11 @@
 package com.tocomfome.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tocomfome.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public abstract List<Produto> findByAtivoTrue();
 }
