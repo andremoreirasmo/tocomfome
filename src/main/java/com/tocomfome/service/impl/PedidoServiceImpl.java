6acb0b624c9fac28c8a85155f656c3937393b5c2
andremoreirasmo <andreluismoreirasmosmo@gmail.com>
2020-06-24 21:23:22 -0300

    Doc: Adicionado anotação service

--- src/main/java/com/tocomfome/service/impl/PedidoServiceImpl.java
+++ src/main/java/com/tocomfome/service/impl/PedidoServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tocomfome.dto.PedidoDTO;
@@ -24,6 +25,7 @@ import com.tocomfome.service.PedidoService;
 import com.tocomfome.service.ProdutoService;
 import com.tocomfome.util.ListUtil;
 
+@Service
 public class PedidoServiceImpl implements PedidoService {
 
 	@Autowired
